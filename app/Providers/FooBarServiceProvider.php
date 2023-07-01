66332b33b2fc8326b14a81922577dc683c0bb7ba
Muhammad Fajar Budi Dharmawan <mfbdofficial1@gmail.com>
2023-07-01 15:36:37 +0700

    materi Service Provider - Membuat Service Provider

--- app/Providers/FooBarServiceProvider.php
+++ app/Providers/FooBarServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Data\Foo;
+use App\Data\Bar;
 
 class FooBarServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,13 @@ class FooBarServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(Foo::class, function($app) {
+            return new Foo();
+        });
+
+        $this->app->singleton(Bar::class, function($app) {
+            return new Bar($app->make(Foo::class));
+        });
     }
 
     /**
